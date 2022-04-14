312e41b8bbe89947346478822ad009bf6354774f
pradeepvkumar222 <pradeep.vkumar222@gmail.com>
2022-04-14 17:57:33 +0530

    Added: Disply meesage

---
+++ test/Data/file.java
@@ -0,0 +1,4 @@
+package com.code.test.Data;
+
+public class file {
+}
---
+++ test/Solution.java
@@ -0,0 +1,8 @@
+package com.code.test;
+
+public class Solution {
+public static void main(String[] args) {
+
+    System.out.println("code_test");
+}
+}
